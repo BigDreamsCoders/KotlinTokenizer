74e3265f12db382ae23e53b34385e54e3828e51c
Nelson Castro <00043516>
2020-10-21 23:36:32 -0600

    Esta mierda funciona

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,13 @@
-import com.sun.xml.internal.ws.model.ParameterImpl
 
 fun main(args: Array<String>){
     val tokens = listOf(
-        Pair("string", """"(.*?)"""".toRegex()),
-        Pair("character", """'(\\'|[^']){1}'""".toRegex()),
-        Pair("data_type", """(?<![a-zA-Z\d._])(int|char|bool)(?![a-zA-Z\d._])""".toRegex()),
-        Pair("access_modifier", """(?<![a-zA-Z\d._])(public|private)(?![a-zA-Z\d._])""".toRegex()),
-        Pair("statement", """(?<![a-zA-Z\d._])(if|else|do|while)(?![a-zA-Z\d._])""".toRegex()),
-        Pair("indentifier", """[_a-zA-Z][a-zA-Z\d_]*""".toRegex()),
-        Pair("number", """(\d)+(.(\d)+)?""".toRegex()),
+        Pair("string", "\"(.*?)\"".toRegex()),
+        Pair("character", "'(\\'|[^']){1}'".toRegex()),
+        Pair("data_type", "(?<![a-zA-Zd._])(int|char|bool)(?![a-zA-Z0-9._])".toRegex()),
+        Pair("access_modifier", "(?<![a-zA-Z0-9._])(public|private|protected)(?![a-zA-Z0-9._])".toRegex()),
+        Pair("statement", "(?<![a-zA-Z0-9._])(if|else|do|while)(?![a-zA-Z0-9._])".toRegex()),
+        Pair("indentifier", "[A-Za-z][A-Za-z0-9_]*".toRegex()),
+        Pair("number", "(0-9)+(.(0-9)+)?".toRegex()),
         Pair("arithmetic_operator", """(\+|-|\*|/|%|\+\+|--)""".toRegex()),
         Pair("logic_operator", """(\|\||&&|(!(?!=)))""".toRegex()),
         Pair("bits_operator", """(>>|<<|&|\||~|\^)""".toRegex()),
@@ -21,14 +20,32 @@ fun main(args: Array<String>){
         Pair("block_end", """;""".toRegex()),
         Pair("expression_start", """\(""".toRegex()),
         Pair("expression_end", """\)""".toRegex()),
-        Pair("whitespace", """\s+""".toRegex()),
+        Pair("arguments", "(\\((?:\\s*[^\\s,]+\\s+[^\\s,]+\\s*,)*\\s*[^\\s,]+\\s+[^\\s,]+\\s*\\))".toRegex())
+    )
+
+    val comments_regex = listOf(
+        Pair("oneline", "\"//.*|(\\\"(?:\\\\\\\\[^\\\"]|\\\\\\\\\\\"|.)*?\\\")|(?s)/\\\\*.*?\\\\*/\"".toRegex()),
+        Pair("multiline", "(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)".toRegex())
     )
 
     val TokensFound = mutableListOf<Pair<String, String>>()
 
-    val c_line = """"hola" 'a' 7.7 ++ int ( ) for"""
+    //val stringInput = readLine()
 
-    val blob = c_line.replace('\n', ' ').split(" ")
+    val c_line = """
+        |int main99 ( char [] args ) {
+        |   /* esta mierda no deberia de salir
+        |   ni esto
+        |   */
+        |   //a ber
+        |   int a = 0;
+        |   return 0 ;
+        |}
+    """.trimMargin()
+
+    val new_c_line = c_line.replace(comments_regex[0].second, "").replace(comments_regex[1].second, "")
+    println(new_c_line);
+    val blob = new_c_line.replace('\n', ' ').split(" ")
 
     blob.forEach outer@{ w ->
         tokens.forEach { token ->
@@ -40,5 +57,9 @@ fun main(args: Array<String>){
                 return@outer
             }
         }
+    }
+
+    TokensFound.forEach{
+        //print(it.first+" ")
     }
 }
